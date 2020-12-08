8e44a956a7f5056bb31da298d0431577e6d6ecad
eforero <torres_1010@hotmail>
2020-12-08 17:01:03 -0500

    clase 4

--- capacitacion2/src/main/java/co/com/capacitacion2/capacitacion2/App.java
+++ capacitacion2/src/main/java/co/com/capacitacion2/capacitacion2/App.java
@@ -1,6 +1,8 @@
 package co.com.capacitacion2.capacitacion2;
 
 import co.com.capacitacion2.capacitacion2clase3.AutomatizacionPaginaWeb;
+import co.com.capacitacion2.capacitacion2clase4.ManagerDriver;
+import co.com.capacitacion2.capacitacion2clase4.PageObjectoTourFrancia;
 
 /**
  * Hello world!
@@ -10,7 +12,25 @@ public class App
 {
     public static void main( String[] args )
     {
-       AutomatizacionPaginaWeb objLanzaPagina=new AutomatizacionPaginaWeb();
-       objLanzaPagina.interactuarConGmail();
+       String urlPagina="https://www.marca.com/ciclismo/tour-francia/clasificacion/etapa-20.html";
+    	ManagerDriver objManager=new ManagerDriver("google", urlPagina);
+    	objManager.navegarAPagina();
+    	PageObjectoTourFrancia objTour =new PageObjectoTourFrancia(objManager.getWebDriver());
+    	objTour.seleccionListaDesplegable("Etapa 17");
+    	esperar(3);
+    	objTour.imprimirlistaDesplegable();
+    	esperar(5);
+    	objManager.cerrarNavegador();
+    	
+    }
+    public static void  esperar(int tiempo) {
+    	
+    	try {
+			Thread.sleep(tiempo*1000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+    	
+       
     }
 }
